b5258bf306f55c70d3ca52850cb6cb46ef35b22b
abiaog <abiaog@126.com>
2015-11-28 17:16:11 +0800

    add bash, csh, git, vim config

--- copy.sh
+++ copy.sh
@@ -3,8 +3,9 @@
 declare -a FILE_IN_REPO
 FILE_IN_REPO=("${FILE_IN_REPO[@]}" ".vimrc")
 FILE_IN_REPO=("${FILE_IN_REPO[@]}" ".gitconfig")
-
+FILE_IN_REPO=("${FILE_IN_REPO[@]}" ".bashrc")
 
 declare -a FILE_IN_REPO_SHADOW
 FILE_IN_REPO_SHADOW=("${FILE_IN_REPO_SHADOW[@]}" "$HOME/.vimrc")
 FILE_IN_REPO_SHADOW=("${FILE_IN_REPO_SHADOW[@]}" "$HOME/.gitconfig")
+FILE_IN_REPO_SHADOW=("${FILE_IN_REPO_SHADOW[@]}" "$HOME/.bashrc")
