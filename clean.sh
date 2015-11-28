0c581b7820a1120ae54871770f51dd49532820a4
abiaog <abiaog@126.com>
2015-11-28 19:54:08 +0800

    add script for regression test and clean

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x # for debug
+
+source copy.sh
+
+length=${#FILE_IN_REPO[@]}
+echo "Remove" $length "File(s):"
+
+for ((i=0; i<$length; i++))
+do
+	echo rm -f ${FILE_IN_REPO_SHADOW[i]}
+	rm -f "${FILE_IN_REPO_SHADOW[i]}"
+	if [ $? -ne 0 ]; then
+		echo "***Failed***"
+		exit 1
+	fi
+done
